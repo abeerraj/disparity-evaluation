e88581c8d79ca98c3af4c1b92eb5cb899941e65e
Ben John <github@benjohn.de>
2016-03-14 00:29:25 +0100

    Still playing around.

--- 1_DisparityAlgorithm/src/OpenCVStereoSGBM.cpp
+++ 1_DisparityAlgorithm/src/OpenCVStereoSGBM.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <opencv2/ximgproc/disparity_filter.hpp>
 #include "OpenCVStereoSGBM.hpp"
 
 void OpenCVStereoSGBM::compute() {
-	cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(-64, 192, 5);
+	//cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(-64, 192, 5);
 
 	int calculatedNumDisparities = ((imgL.cols / 8) + 15) & -16;
 	//Bitweise Maskierung mit -16 beduetet dass die 4 letzten Bits zu 0 werden
@@ -14,6 +15,7 @@ void OpenCVStereoSGBM::compute() {
 	int numberOfDisparities = calculatedNumDisparities;
 
 	//cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(0, 256, 5);
+	cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(-48, 128, 5);
 	sgbm->setPreFilterCap(63);
 	sgbm->setUniquenessRatio(10);
 	sgbm->setSpeckleWindowSize(100);
@@ -22,14 +24,39 @@ void OpenCVStereoSGBM::compute() {
 	sgbm->setMode(cv::StereoSGBM::MODE_HH);
 	sgbm->setP1(600);
 	sgbm->setP2(2400);
-	sgbm->compute(imgL, imgR, getResult());
+	//sgbm->compute(imgL, imgR, getResult());
+
+	// WLS filtering with left-to-right / right-to-left cross-checking
+	cv::Mat disp, dispR;
+
+	cv::Ptr<cv::ximgproc::DisparityWLSFilter> wls_filter = cv::ximgproc::createDisparityWLSFilter(sgbm);
+	cv::Ptr<cv::StereoMatcher> sgbmR = cv::ximgproc::createRightMatcher(sgbm);
+	sgbm->compute(imgL, imgR, disp);
+	sgbmR->compute(imgR, imgL, dispR);
+
+	cv::Mat filteredDisp;
+
+	wls_filter->setLambda(8000.0);
+	wls_filter->setSigmaColor(1.5);
+	wls_filter->filter(disp, imgL, filteredDisp, dispR);
+
+	getResult() = filteredDisp;
 
 	cv::Mat out;
 	getResult().convertTo(out, CV_32F, 1.0 / 16);
 	getResult() = out;
+	out.setTo(NAN, out == -1);
+	cv::SparseMat S = cv::SparseMat(out);
+
+	//cv::namedWindow("Display frame", CV_WINDOW_NORMAL);
+	cv::Mat normalizedMat;
+	normalize(filteredDisp, normalizedMat, 0, 255, CV_MINMAX, CV_8U);
+	cv::imshow("Display frame", normalizedMat );
+	//cv::resizeWindow("Display frame", 1280, 720);
+	cv::waitKey(0);
 
 	double min, max;
-	cv::minMaxLoc(getResult(), &min, &max);
+	cv::minMaxLoc(S, &min, &max);
 	std::cout << "computedDisp min: " << min << " max: " << max << std::endl;
 
 	/*// Save the image data in binary format
@@ -38,7 +65,7 @@ void OpenCVStereoSGBM::compute() {
 	os.write((char*)out.data,out.step.p[0]*out.rows);
 	os.close();*/
 
-	std::cout << "M = "<< std::endl << " "  << getResult().row(0) << std::endl << std::endl;
+	std::cout << "M = " << std::endl << " " << getResult().row(0) << std::endl << std::endl;
 
 	//cv::minMaxLoc(getResult(), &min, &max);
 	//std::cout << "computedDisp / 16 min: " << min << " max: " << max << std::endl;
